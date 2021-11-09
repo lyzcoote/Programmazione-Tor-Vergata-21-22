e3eddd52f5046ba136e4d1e52a81ecbca30eeb64
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2021-11-09 15:21:01 +0100

    Default variabile di passaggio della funzione

--- lezione_9_nov.py
+++ lezione_9_nov.py
@@ -6,13 +6,13 @@ x = float(input('Inserisci il valore di X: '))
 y = float(input('Inserisci il valore di Y: '))
 
 
-def radice_quadrata(a):
-    g = 5.0
-    while abs(g * g - a) > 0.00001:
+def radice_quadrata(a, err = 0.000001):
+    g = a/2
+    while abs(g * g - a) > err:
         g = 0.5 * (g + a/g)
     return g
 
-r_x, r_y = radice_quadrata(x), radice_quadrata(y)
+r_x, r_y = radice_quadrata(x, 0.1), radice_quadrata(y)
 
 print('La radice quadrata di X è:', r_x)
 print('La radice quadrata di Y è:', r_y)
