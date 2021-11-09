6d48781b234444e75747bee87a017eaa9781c2d4
Lyz Coote <44366896+lyzcoote@users.noreply.github.com>
2021-11-09 14:39:30 +0100

    Utilizzo delle funzioni per la radice quadrata

--- lezione_9_nov.py
+++ lezione_9_nov.py
@@ -5,22 +5,15 @@ Lezione 9 Novembre 2021
 x = float(input('Inserisci il valore di X: '))
 y = float(input('Inserisci il valore di Y: '))
 
-g_x = 5.0
-
-while abs(g_x * g_x - x) > 0.00001:
-    g_x = 0.5 * (g_x + x/g_x)
-
-g_y = 5.0
-
-while abs(g_y * g_y - y) > 0.00001:
-    g_y = 0.5 * (g_y + y/g_y)
-
-print('La radice quadrata di X è:', g_x)
-print('La radice quadrata di Y è:', g_y)
-print('La radice quadrata di X + Y è:', g_x + g_y)
 
 def radice_quadrata(a):
     g = 5.0
     while abs(g * g - a) > 0.00001:
         g = 0.5 * (g + a/g)
     return g
+
+r_x, r_y = radice_quadrata(x), radice_quadrata(y)
+
+print('La radice quadrata di X è:', r_x)
+print('La radice quadrata di Y è:', r_y)
+print('La radice quadrata di X + Y è:', r_x + r_y)
